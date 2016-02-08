b00e8dee7ffe092a2d4fd3e3ce5b4a8d48e96467
derwasp <nbcwasp@gmail.com>
2016-02-08 21:17:06 +0100

    Unreal Commander package

---
+++ unreal-commander/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'unreal-commander'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://x-diesel.org/download/evolution/uncomsetup2.02(build1107).exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'Unreal Commander*'
+}
+
+Install-ChocolateyPackage @packageArgs
