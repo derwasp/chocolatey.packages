3814ae70dc8ab4e146fd3c21a8169e7f8bee22b0
derwasp <nbcwasp@gmail.com>
2017-02-12 15:10:32 +0100

    added checksum to uncom; changed icon url to use cdn.

--- unreal-commander/tools/chocolateyinstall.ps1
+++ unreal-commander/tools/chocolateyinstall.ps1
@@ -3,15 +3,16 @@
 $packageName= 'unreal-commander'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://x-diesel.com/download/evolution/uncomsetup2.02(build1111).exe'
+$checksum   = '48D10680EB002A17929CC141E354C6F3'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-
+  checksum      = $checksum
   validExitCodes= @(0, 3010, 1641)
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
   softwareName  = 'Unreal Commander*'
 }
