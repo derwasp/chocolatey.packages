21b80316259b7e7349e9356f80ed3111db1e9b7f
derwasp <nbcwasp@gmail.com>
2017-03-28 10:06:32 +0200

    uncom 3.57 (1205)

--- unreal-commander/tools/chocolateyinstall.ps1
+++ unreal-commander/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'unreal-commander'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://x-diesel.com/download/evolution/uncomsetup3.57x32(build1201).exe'
-$checksum   = '59AA3C962A26937A0F062CD39A93351F'
+$url        = 'http://x-diesel.com/download/evolution/uncomsetup3.57x32(build1205).exe'
+$checksum   = 'B5427F758ED8909AB9071137960868F7'
 
 $packageArgs = @{
   packageName   = $packageName
