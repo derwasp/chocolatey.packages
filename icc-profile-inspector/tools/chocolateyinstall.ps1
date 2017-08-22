6c071d320f4fc269b60a1ef074cecdbae53612be
Denys Zhuravel <Denys.Zhuravel@albelli.com>
2017-08-22 09:51:03 +0000

    icc-profile-inspector changed the package

--- icc-profile-inspector/tools/chocolateyinstall.ps1
+++ icc-profile-inspector/tools/chocolateyinstall.ps1
@@ -1,22 +1,24 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'icc-profile-inspector' # arbitrary name for the package, used in messages
+$packageName= 'icc-profile-inspector'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.color.org/ICCProfileInspector2_4.zip' # download url, HTTPS preferred
+$url        = 'http://www.color.org/ICCProfileInspector2_4.zip'
 
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-
-$pp = Get-PackageParameters
-$installDir = $toolsPath
-if ($pp.InstallDir -or $pp.InstallationPath ) { $InstallDir = $pp.InstallDir + $pp.InstallationPath }
-Write-Host "ICC Profile Inspector is going to be installed in '$installDir'"
+$fileLocation = Join-Path $toolsDir 'ICCProfileInspector.exe'
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $installDir
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
   url           = $url
+  softwareName  = 'ICC Profile Inspector 2.4.0'
+  file          = $fileLocation
+
   checksum      = '0A443E5F6FA7B26F19513C49F5A703BA'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
 }
 
 Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
